e1bbba022521a8ef0566061be990ec5cec5e28f8
niklas.leopold <leopold.niklas@gmail.com>
2017-07-09 10:01:03 +0200

    initial commit

---
+++ src/main/java/com/github/morotsman/bevarage/beverage/BeverageApplication.java
@@ -0,0 +1,12 @@
+package com.github.morotsman.bevarage.beverage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeverageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeverageApplication.class, args);
+	}
+}
